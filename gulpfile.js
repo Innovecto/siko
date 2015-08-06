d5920d06f94c2c0b5ac1dfc41014b3db6713be1d
Novan Adrian <novanadrian@gmail.com>
2015-08-06 23:14:50 +0700

    Upgrade to Laravel 5.1

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,31 @@ var elixir = require('laravel-elixir');
  |--------------------------------------------------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
+ | for your Laravel application. By default, we are compiling the Sass
  | file for our application, as well as publishing vendor resources.
  |
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+    mix
+        .sass([
+            'vendor/*.scss'
+        ], 'public/assets/css/vendor.min.css')
+        .sass([
+            'plugin/*.scss' 
+        ], 'public/assets/css/plugin.min.css')
+        .sass([
+            'app.scss'
+        ], 'public/assets/css/app.min.css')
+        .scripts([
+            'vendor/*.js'
+        ], 'public/assets/js/vendor.min.js')
+        .scripts([
+            'plugin/*.js'
+        ], 'public/assets/js/plugin.min.js')
+        .scripts([
+            'app.js',
+            'component/*.js'
+        ], 'public/assets/js/app.min.js')
+    ;
 });
