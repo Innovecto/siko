f4004981a63351e917d1b4f797f706cca8c1a5cb
Novan Adrian <novanadrian@gmail.com>
2015-08-10 17:17:29 +0700

    Adding fullname attribute

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -7,25 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     protected $fillable = [
-    	'id_number', 
-    	'first_name', 
-    	'last_name', 
-    	'birthdate', 
-    	'birthplace', 
-    	'gender', 
-    	'religion_id', 
-    	'occupation', 
-    	'marital_status', 
-    	'province', 
-    	'city', 
-    	'subdistrict', 
-    	'village', 
-    	'cluster', 
-    	'subcluster', 
-    	'email', 
-    	'password'
+        'id_number',
+        'first_name',
+        'last_name',
+        'birthdate',
+        'birthplace',
+        'gender',
+        'religion_id',
+        'occupation',
+        'marital_status',
+        'province',
+        'city',
+        'subdistrict',
+        'village',
+        'cluster',
+        'subcluster',
+        'email',
+        'password',
     ];
 
+    public function getFullnameAttribute()
+    {
+        return !empty($this->attributes['last_name']) ? $this->attributes['first_name'] . ' ' . $this->attributes['last_name'] : $this->attributes['first_name'];
+    }
 
-    
 }
